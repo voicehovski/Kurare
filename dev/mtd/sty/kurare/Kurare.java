23a8d27f62995de0c27aa22348b3a8d546a3cf04
Voicehovski <help.kiev@gmail.com>
2022-11-01 12:08:01 +0200

    Create packages and directory structure Create directory for classes and modify build scripts to use it and packages

--- dev/mtd/sty/kurare/Kurare.java
+++ dev/mtd/sty/kurare/Kurare.java
@@ -1,3 +1,5 @@
+package dev.mtd.sty.kurare;
+
 public class Kurare {
 	public static void main (String [] args) {
 		System .out .println ( "Ok" );
--- run.cmd
+++ run.cmd
@@ -1,1 +1,6 @@
-java Kurare
+: -cp|-classpath|--class-path
+: -Dname=value	Передавать аргументы. Чем отличаются от аргументов командной строки?
+: -verbose:[class|module|gc|jni]	Показывает что загружается, но не совсем понятно. По умолчанию class
+: @argument files	Опции можно записывать в файл
+
+java -cp classes dev.mtd.sty.kurare.Kurare
