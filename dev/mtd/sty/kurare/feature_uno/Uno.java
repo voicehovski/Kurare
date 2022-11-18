711be0aa2cf7a5e384867afdb9696949f7ebe46b
Voicehovski <help.kiev@gmail.com>
2022-11-18 21:41:28 +0200

    Add consturctor with parameter and default

--- dev/mtd/sty/kurare/feature_uno/Uno.java
+++ dev/mtd/sty/kurare/feature_uno/Uno.java
@@ -4,7 +4,17 @@ package dev.mtd.sty.kurare.feature_uno;
 public class Uno {
 	public static final String TEMPLATE = "[%d]";
 	
+	private String template;
+	
+	public Uno ( String template ) {
+		this .template  = template;
+	}
+	
+	public Uno (  ) {
+		this .template = TEMPLATE;
+	}
+	
 	public String action (int i) {
-		return String .format ( TEMPLATE, i );
+		return String .format ( template, i );
 	}
 }
